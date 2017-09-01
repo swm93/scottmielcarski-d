84862a092fd9ce9fb1971a2dd4297e3ad1ba2f0c
Scott Mielcarski <scott_mielcarski@me.com>
2017-09-01 14:51:19 -0700

    Use standard naming convensions in files and directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
-import { VersionControlGraphModule } from './shared/version_control_graph/version_control_graph.module';
+import { VersionControlGraphModule } from './shared/version-control-graph/version-control-graph.module';
 
 
 
