b5ebb81c963370151946fc6b0c6f4845f9c301c2
Scott Mielcarski <scott_mielcarski@me.com>
2017-09-03 11:38:40 -0700

    Add ng2-pdf-viewer library

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -25,7 +25,9 @@
 
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      'ng2-pdf-viewer': 'node_modules/ng2-pdf-viewer',
+      'pdfjs-dist': 'node_modules/pdfjs-dist'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -38,6 +40,13 @@
         }
       },
       rxjs: {
+        defaultExtension: 'js'
+      },
+      'ng2-pdf-viewer': {
+        main: 'dist/index.js',
+        defaultExtension: 'js'
+      },
+      'pdfjs-dist': {
         defaultExtension: 'js'
       }
     }
